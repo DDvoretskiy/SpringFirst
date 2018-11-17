84ad02ba157f7be860f08d43191b1ef860c7fef4
DDvoretskiy <9554555@gmail.com>
2018-11-17 19:57:56 +0300

    лекция 8

--- src/main/java/by/izone/spring/core/App.java
+++ src/main/java/by/izone/spring/core/App.java
@@ -5,26 +5,47 @@ import by.izone.spring.core.beans.Client;
 import by.izone.spring.core.beans.Event;
 import by.izone.spring.core.beans.EventType;
 import by.izone.spring.core.loggers.EventLogger;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import by.izone.spring.core.spring.AppConfig;
+import by.izone.spring.core.spring.LoggerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
-
+@Service
 public class App {
+
+    @Autowired
     private Client client;
+
+    @Resource(name = "defaultLogger")
     private EventLogger defaultLogger;
+
+    @Resource(name = "loggerMap")
     private Map<EventType, EventLogger> loggers;
 
-    public App(Client client, EventLogger defaulteLogger, Map<EventType, EventLogger> loggers) {
+    public App() {
+    }
+
+    App(Client client, EventLogger defaultLogger,
+        Map<EventType, EventLogger> loggersMap) {
         this.client = client;
-        this.defaultLogger = defaulteLogger;
-        this.loggers = loggers;
+        this.defaultLogger = defaultLogger;
+        this.loggers = loggersMap;
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class, LoggerConfig.class);
+        ctx.scan("by.izone.spring.core");
+        ctx.refresh();
+
         App app = (App) ctx.getBean("app");
+
+        Client client = ctx.getBean(Client.class);
+        System.out.println("Client says: " + client.getGreeting());
 
         Event event = ctx.getBean(Event.class);
         app.logEvent(EventType.INFO, event, "Some event for 1");
